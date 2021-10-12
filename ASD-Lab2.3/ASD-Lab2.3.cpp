6c041746b129e6fd52dfc419c117467e87cdf776
Kurin, Alex <olexkurin@gmail.com>
2021-10-12 23:30:43 +0300

    lab2.3

---
+++ ASD-Lab2.3/ASD-Lab2.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void OutputRange(int A, int B)
+{
+	std::cout << A << ' ';
+	if (A == B)
+	{
+		return;
+	}
+	else if (A < B)
+	{
+		OutputRange(A + 1, B);
+	}
+	else
+	{
+		OutputRange(A - 1, B);
+	}
+}
+
+int main()
+{
+	OutputRange(8, -1);
+	std::cin.get();
+}
