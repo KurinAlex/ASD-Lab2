d1b62d493f84ac2f55aeea656d2c5c1bb2b70853
Kurin, Alex <olexkurin@gmail.com>
2021-10-13 01:12:43 +0300

    lab2.2 add helpers

---
+++ ASD-Lab2.2/helpers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include"helpers.h"
+
+int EnterInteger(const char* integer_name)
+{
+	int number;
+
+	bool isFailure = true;
+	do
+	{
+		std::cout << "Enter " << integer_name << ": ";
+		std::cin >> number;
+		isFailure = std::cin.fail();
+
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+	} while (isFailure);
+
+	return number;
+}
